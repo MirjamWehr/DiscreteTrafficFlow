f624aaa3020071b619641399e88674bfc9dbe1a2
Mirjam Wehr <mirjam.wehr@googlemail.com>
2018-01-18 09:24:23 +0100

    diffrent initial values, adapted to the ones I calculated through

--- Homogen/godunov.m
+++ Homogen/godunov.m
@@ -1,4 +1,4 @@
-H = 1;
+H = 1.1;
 %number of cells
 N = 100;
 a = 0;
@@ -7,11 +7,12 @@ dx = (b-a)/N;
 t = 0;
 
 x = (a+dx/2:dx:b-dx/2);
+xrieman = x-1/2;
 
-ql = 1/8;
+ql = 1/4;
 qr = 1/4;
-pl = 1/2;
-pr = 1/4;
+pl = 1/4;
+pr = 1/2;
 u0 = ones(2,N);
 u0(1,1:50) = pl*u0(1,1:50);
 u0(2,1:50) = ql*u0(2,1:50);
@@ -39,12 +40,19 @@ u = godunovStep(u0,dt,dx,H);
 for i = 1:2*N
     
     t = t+dt;
+    analyticalU = analyticalSolutionValue(t,[pl;ql],[pr;qr],H,xrieman);
     plot_p = subplot(2,1,1);
     plot(plot_p,x,u(1,:),'x');
+    hold on
+    plot(plot_p,x,analyticalU(1,:),'-');
+    hold off
     title(['t = ',num2str(t)]);
     title(plot_p,{['Time ',num2str(t)];'Values for \rho'})
     plot_q = subplot(2,1,2);
     plot(plot_q,x,u(2,:),'x');
+    hold on
+    plot(plot_q,x,analyticalU(2,:),'-');
+    hold off
     title(plot_q,'Values for q')
     axis([plot_p,plot_q],[a b 0 0.6])
     
