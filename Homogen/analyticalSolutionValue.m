a79720c023613b0debccee3b9cdc680ee88427c8
Mirjam Wehr <mirjam.wehr@googlemail.com>
2018-01-18 09:25:43 +0100

    first version of the calculation of the analytical solution. Problem: differs clearly from the numerical solution

--- Homogen/analyticalSolutionValue.m
+++ Homogen/analyticalSolutionValue.m
@@ -8,20 +8,33 @@ function [ u ] = analyticalSolutionValue( t,ul,ur,H,x )
 %   x, 1xN matrix, the x axes
 %   u, 2xN matrix, the solution at time t
 
+N = length(x);
+u = zeros(2,N);
+
 m = getM(H,ul,ur);
 
 %check where we need rarefaction and where we need shock
 %between ul an um
 if ul(1) < m(1)
     %rarefaction between ul and um
+    lambda1l = -H*ul(2)/(1-ul(1));
+    lambda1m = -H*m(2)/(1-m(1));
+    u = u + ul*(x<=lambda1l*t);
+    u = u + m*((x>lambda1m*t)&(x<t));
+    %connection from ul to um
+    xil = - H*ul(2)/(1-ul(1));
+    index = find((x>lambda1l*t)&(x<=lambda1m*t));
+    u(1,index) = 1/t*x(index) - xil + ul(1);
+    u(2,index) = (1-1/t*x(index)+xil-ul(1)).^H * ul(2)/((1-ul(1))^H);
 else
-    %shock between ul and um - tritt nicht ein?
+    %shock between ul and um so lambda1(ul)>=lambda1(um)
+    lambda1 = -H*m(2)/(1-m(1));
+    u = u + ul*(x<=lambda1*t);
+    u = u + m*((x>lambda1*t)&(x<t));
 end
-if m(1) < ur(1)
-    %rarefaction between um and ur - tritt nicht ein?
-else
-    %shock between um and ur
-end
+%not necessary for um and ur to compare since there is always a contact
+%discontinuity
+u = u + ur*(x>=t);
 
 
 
