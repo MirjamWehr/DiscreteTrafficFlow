e3fc8c3c4c7acdc13f7c13ab3eb80134ac356df1
Mirjam Wehr <mirjam.wehr@googlemail.com>
2018-03-05 11:23:12 +0100

    keep values admissible
    Problem: for large values phat2 and phat3 the upper_bound is not reachable
    
    solved by choosing the minimum of 1 and the maximal possible value
    
    however solved first for pbar2 then for pbar3 so the flux is not shared even
    Aim: try to solve such that qhat2 and qhat3 as similar as possible

--- Crossroads/getUbar.m
+++ Crossroads/getUbar.m
@@ -11,9 +11,14 @@ if q1==0
 else
     %the upper bound for q1 respectively q2+q3
     upper_bound = q1/(q1-p1+1);
-    u1=[1-(1-p1)/(1-p1+q1);upper_bound];
-    u2=[upper_bound/2+p2-q2;upper_bound/2];
-    u3=[upper_bound/2+p3-q3;upper_bound/2];
+    frac = (1-p1)/q1;
+    d2 = p2-q2;
+    d3 = p3-q3;
+    pbar2 = min(1,d2+upper_bound);
+    pbar3 = min(1,upper_bound+d2+d3-pbar2);
+    u1=[pbar2*(-frac)+pbar3*(-frac)+1+(d2+d3)*frac;pbar2+pbar3-d2-d3];
+    u2=[pbar2;pbar2-d2];
+    u3=[pbar3;pbar3-d3];
 end
 
 %theoretisch kann man beide fälle verbinden es gibt aber falls q1=0 und
