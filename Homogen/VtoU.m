1a6d8e29b341515f4382a44d97a7eb06274ced44
Mirjam Wehr <mirjam.wehr@googlemail.com>
2018-03-09 11:20:18 +0100

    fixed one part of the eps problem
    Problem: need to use eps for some calculations in UtoV and need to use eps for the same retransformation in VtoU
    Solved: introduce index in UtoV where the information about the use of eps is saved and give index to VtoU
    adapted these changes where I used VtoU
    
    NewProblem: u is shifted and so is the use of index
    Solved: take all diffrent resulting indexes and connect them with or
    
    Fazit: seems to arise a new problem in the results
    results don't seem to be apropriate

--- Homogen/VtoU.m
+++ Homogen/VtoU.m
@@ -1,4 +1,4 @@
-function [ u ] = VtoU( v,H )
+function [ u ] = VtoU( v,H,index )
 %VtoU converts the variable v=(p,z)  to u=(p,q)
 %   v 2xN matrix
 %   H scalar, function parameter
@@ -8,7 +8,6 @@ u = zeros(size(v));
 p = v(1,:);
 z = v(2,:);
 u(1,:)=p;
-pones = p==1;
-u(2,:)=1/H*z.*(1-p).^H;
-u(2,pones)=z(pones)*eps;
+u(2,index)=z(index)*eps;
+u(2,~index)=z(~index).*((1-p(~index)).^H)/H;
 end
