3c8371fe235c9c307c96b1d51021383c971ef6ee
Mirjam Wehr <mirjam.wehr@googlemail.com>
2018-06-13 20:00:21 +0200

    algorithm implemented
    implemented the algorithm that calculates the correct values for the junction
    Model 1 is done

--- Crossroads/getUbar.m
+++ Crossroads/getUbar.m
@@ -13,14 +13,40 @@ else
     d2 = p2-q2;
     d3 = p3-q3;
     %Setting1 Cars spread evenly
-    x = min([1-d2,1-d3,q1/(2*(q1-p1+1))]);
-    u1=[1-2*x*((1-p1)/q1);2*x];
-    u2=[d2+x;x];
-    u3=[d3+x;x];
+%     x = min([1-d2,1-d3,q1/(2*(q1-p1+1))]);
+%     u1=[1-2*x*((1-p1)/q1);2*x];
+%     u2=[d2+x;x];
+%     u3=[d3+x;x];
     %Setting2 1.max flux 2.spread evenly
 %     x = min(1-d2,q1/(2*(q1-p1+1)));
 %     y = min(1-d3,q1/(2*(q1-p1+1)));
 %     u1=[1-(x+y)*(1-p1/q1);2*x];
 %     u2=[d2+x;x];
 %     u3=[d3+y;y];
+    %Algorithm
+    ax = 1-d2;
+    ay = 1-d3;
+    c = q1/(q1-p1+1);
+    if ax < ay
+        if ax < c/2
+            x=ax;
+            c=c-ax;
+            y= min(c,ay);
+        else
+            x=c/2;
+            y=c/2;
+        end
+    else
+        if ay < c/2
+            y=ay;
+            c=c-ay;
+            x=min(c,ax);
+        else
+            x=c/2;
+            y=c/2;
+        end
+    end
+    u1=[1-(x+y)*(1-p1/q1);2*x];
+    u2=[d2+x;x];
+    u3=[d3+y;y];       
 end
